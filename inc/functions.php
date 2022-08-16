e0e6805c8ea55aa4c4f198bbf6dda1b027aa6597
Tiago Murakami <trmurakami@gmail.com>
2022-08-16 04:20:35 -0300

    Create index

--- inc/functions.php
+++ inc/functions.php
@@ -15,6 +15,24 @@ if ($debug) {
     error_reporting(E_ALL);
 }
 
+/* Connect to Elasticsearch */
+try {
+    $client = \Elasticsearch\ClientBuilder::create()->setHosts($hosts)->build(); 
+    //print("<pre>".print_r($client,true)."</pre>");
+    $indexParams['index']  = $index;   
+    $testIndex = $client->indices()->exists($indexParams);
+} catch (Exception $e) {    
+    $error_connection_message = '<div class="alert alert-danger" role="alert">Elasticsearch não foi encontrado. Favor executar o arquivo elasticsearch.lnk.</div>';
+}
+/* Create index if not exists */
+if (isset($testIndex) && $testIndex == false) {
+    Elasticsearch::createIndex($index, $client);
+    sleep(10);
+    Elasticsearch::mappingsIndex($index, $client);
+    sleep(10);
+    Elasticsearch::createIndex($indexAdm, $client);
+}
+
 
 /**
  * Class Inicio
