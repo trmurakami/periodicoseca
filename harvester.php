4e7f679b3c75763910a1465ea757f689915fc5fa
Tiago Murakami <trmurakami@gmail.com>
2023-03-17 23:03:14 -0300

    Correcao

--- harvester.php
+++ harvester.php
@@ -194,8 +194,8 @@ if (isset($_GET["oai"])) {
         }
         foreach ($recs as $rec) {
             $data = $rec->metadata->children('https://www.openarchives.org/OAI/2.0/oai_dc/');
-            $rows = $data->children('https://purl.org/dc/elements/1.1/');            
-
+            $rows = $data->children('https://purl.org/dc/elements/1.1/');
+            
             if (isset($rows->publisher)) {
                 $query["doc"]["isPartOf"]["publisher"]["organization"]["name"] = (string)$rows->publisher;
             }
