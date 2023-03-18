efc9d210b2b2445bc6712abd85046174a5afeca7
Tiago Murakami <trmurakami@gmail.com>
2023-03-17 22:31:32 -0300

    Correcao

--- harvester.php
+++ harvester.php
@@ -194,7 +194,7 @@ if (isset($_GET["oai"])) {
         }
         foreach ($recs as $rec) {
             $data = $rec->metadata->children('https://www.openarchives.org/OAI/2.0/oai_dc/');
-            if(empty($data->children('https://purl.org/dc/elements/1.1/'))){         
+            if(is_null($data->children('https://purl.org/dc/elements/1.1/'))){         
                 continue;      
             } else {
                 $rows = $data->children('https://purl.org/dc/elements/1.1/');
